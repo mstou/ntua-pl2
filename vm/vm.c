211b3045cb5c4776cd2c906b36341677d2ff5d1e
Miltiadis Stouras <m.stouras@gmail.com>
2019-12-09 13:58:07 +0200

    Finish instructions implementation [vm]

--- vm/vm.c
+++ vm/vm.c
@@ -2,8 +2,8 @@
 
 #define STACK_SIZE 1<<20
 #define NEXT_INSTRUCTION goto next_instruction
-#define ROWS 80
-#define COLS 25
+#define ROWS 25
+#define COLS 80
 
 char code[ROWS][COLS]; // the array is initialized to '\0'
 // we will treat '\0' as the space command, which does nothing,
@@ -15,6 +15,7 @@ void loader(){
   int i = 0, j = 0;
   char c;
   while((c = getchar_unlocked()) != EOF){
+    // printf("%c",c);
     if(c == '\n'){
       i = (i+1)%ROWS;
     }
@@ -36,74 +37,77 @@ int main(void){
   register int x_direction = 0, y_direction = 1; // current direction in each axis
   register int top = -1; // top points to the top element of the stack
   register unsigned long int a,b; // local varriables to use for items that we pop out of the stack
+  char c;
+  int x;
 
   // TODO : implement direct threading & make PC a pointer
+  // -- change pop implementation to help branch prediction on (top==-1)
   // reminder: GNU C allows global register values
 next_instruction:
   switch(code[pc_x][pc_y]){
-    case '+':
-      if( top == -1 ) a = 0;
-      else a = stack[top--];
-      if( top == -1 ) b = 0;
-      else b = stack[top--];
-
-      stack[++top] = a+b;
+    case '+': // add
+    label_add:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      b = (top==-1) ? 0 : stack[top--];
+      stack[++top] = b+a;
       NEXT_INSTRUCTION;
 
-    case '-':
-      if( top == -1 ) a = 0;
-      else a = stack[top--];
-      if( top == -1 ) b = 0;
-      else b = stack[top--];
-
-      stack[++top] = a-b;
+    case '-': // substract
+    label_substract:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      b = (top==-1) ? 0 : stack[top--];
+      stack[++top] = b-a;
       NEXT_INSTRUCTION;
 
-    case '*':
-      if( top == -1 ) a = 0;
-      else a = stack[top--];
-      if( top == -1 ) b = 0;
-      else b = stack[top--];
-
-      stack[++top] = a*b;
+    case '*': // multiply
+    label_multiply:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      b = (top==-1) ? 0 : stack[top--];
+      stack[++top] = b*a;
       NEXT_INSTRUCTION;
 
-    case '/':
-      if( top == -1 ) a = 0;
-      else a = stack[top--];
-      if( top == -1 ) b = 0;
-      else b = stack[top--];
-
-      stack[++top] = a/b;
+    case '/': // divide
+    label_divide:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      b = (top==-1) ? 0 : stack[top--];
+      stack[++top] = b/a;
       NEXT_INSTRUCTION;
 
-    case '%':
-      if( top == -1 ) a = 0;
-      else a = stack[top--];
-      if( top == -1 ) b = 0;
-      else b = stack[top--];
-
-      stack[++top] = a%b;
+    case '%': // modulo
+    label_modulo:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      b = (top==-1) ? 0 : stack[top--];
+      stack[++top] = b%a;
       NEXT_INSTRUCTION;
 
-    case '!':
-      if( top == -1 ) a = 0;
-      else a = stack[top--];
-      if( a != 0 ) stack[++top] = 0;
-      else stack[++top] = 1;
+    case '!': // not
+    label_not:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      stack[++top] = (a!=0) ? 0 : 1;
       NEXT_INSTRUCTION;
 
-    case '`':
-      if( top == -1 ) a = 0;
-      else a = stack[top--];
-      if( top == -1 ) b = 0;
-      else b = stack[top--];
-
-      a = (a > b) ? 1 : 0;
-      stack[++top] = a;
+    case '`': // greater
+    label_greater:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      b = (top==-1) ? 0 : stack[top--];
+      stack[top++] = (b > a) ? 1 : 0;
       NEXT_INSTRUCTION;
 
-    case '>':
+    case '>': // right
     label_right:
       x_direction = 0;
       y_direction = 1;
@@ -111,7 +115,7 @@ next_instruction:
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       NEXT_INSTRUCTION;
 
-    case '<':
+    case '<': // left
     label_left:
       x_direction = 0;
       y_direction = -1;
@@ -119,7 +123,7 @@ next_instruction:
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       NEXT_INSTRUCTION;
 
-    case '^':
+    case '^': // up
     label_up:
       x_direction = -1;
       y_direction = 0;
@@ -127,7 +131,7 @@ next_instruction:
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       NEXT_INSTRUCTION;
 
-    case 'v':
+    case 'v': // down
     label_down:
       x_direction = 1;
       y_direction = 0;
@@ -135,7 +139,8 @@ next_instruction:
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       NEXT_INSTRUCTION;
 
-    case '?':
+    case '?': // random
+    label_random:
       a = random();
       switch(a){
         case 0:
@@ -148,38 +153,178 @@ next_instruction:
           goto label_up;
       }
 
-    case '_':
-      if( top == -1 ) a = 0;
-      else a = stack[top--];
-
+    case '_': // horizontal if
+    label_horizontal_if:
+      a = (top==-1) ? 0 : stack[top--];
       if(a == 0) goto label_right;
       else goto label_left;
 
-    case '|':
-      if( top == -1 ) a = 0;
-      else a = stack[top--];
-
+    case '|': // vertical if
+    label_vertical_if:
+      a = (top==-1) ? 0 : stack[top--];
       if(a == 0) goto label_down;
       else goto label_up;
 
-    case '"':
-    label_stringmode_on:
-    label_stringmode_off:
+    case '"': // stringmode
+      label_stringmode:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
 
-    case ':':
-    case '\\':
-    case '$':
-    case '.':
-    case ',':
-    case '#':
-    case 'g':
-    case 'p':
-    case '&':
-    case '~':
-    case '@':
+      c = code[pc_x][pc_y];
+      if( c != '"'){
+        stack[++top] = c;
+        goto label_stringmode;
+      }
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      NEXT_INSTRUCTION;
+
+    case ':': // dup
+    label_dup:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top];
+      stack[++top] = a;
+      NEXT_INSTRUCTION;
+
+    case '\\': // swap
+    label_swap:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      b = (top==-1) ? 0 : stack[top--];
+      stack[++top] = a;
+      stack[++top] = b;
+      NEXT_INSTRUCTION;
+
+    case '$': // pop
+    label_pop:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      if(top != -1) top--;
+      NEXT_INSTRUCTION;
+
+    case '.': // output int
+    label_output_int:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      printf("%ld ",a);
+      NEXT_INSTRUCTION;
+
+    case ',': // output char
+    label_output_char:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      printf("%c", (char) a);
+      NEXT_INSTRUCTION;
+
+    case '#': // bridge
+    label_bridge:
+      pc_x = ((pc_x + 2*x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + 2*y_direction)%COLS + COLS)%COLS;
+      NEXT_INSTRUCTION;
+
+    case 'g': // get
+    label_get:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      b = (top==-1) ? 0 : stack[top--];
+      stack[++top] = code[a][b];
+      NEXT_INSTRUCTION;
+
+    case 'p': // put
+    label_put:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      a = (top==-1) ? 0 : stack[top--];
+      b = (top==-1) ? 0 : stack[top--];
+      x = (top==-1) ? 0 : stack[top--];
+      code[a][b] = (char) x;
+      NEXT_INSTRUCTION;
+
+    case '&': // input int
+    label_input_int:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      scanf("%d",&x);
+      stack[top++] = x;
+      NEXT_INSTRUCTION;
+
+    case '~': // input_character
+    label_input_character:
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      scanf("%c",&c);
+      stack[top++] = c;
+      NEXT_INSTRUCTION;
+
+    case '0':
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      stack[++top] = 0;
+      NEXT_INSTRUCTION;
+
+    case '1':
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      stack[++top] = 1;
+      NEXT_INSTRUCTION;
+
+    case '2':
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      stack[++top] = 2;
+      NEXT_INSTRUCTION;
+
+    case '3':
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      stack[++top] = 3;
+      NEXT_INSTRUCTION;
+
+    case '4':
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      stack[++top] = 0;
+      NEXT_INSTRUCTION;
+
+    case '5':
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      stack[++top] = 5;
+      NEXT_INSTRUCTION;
+
+    case '6':
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      stack[++top] = 6;
+      NEXT_INSTRUCTION;
+
+    case '7':
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      stack[++top] = 7;
+      NEXT_INSTRUCTION;
+
+    case '8':
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      stack[++top] = 8;
+      NEXT_INSTRUCTION;
+
+    case '9':
+      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
+      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
+      stack[++top] = 9;
+      NEXT_INSTRUCTION;
+
+    case '@': // end
+    label_end:
       return 0;
   }
-
 
 
   return 0;
