ec1cbae19ddeed04d63d155db017800011354eb9
Miltiadis Stouras <m.stouras@gmail.com>
2019-12-21 23:42:14 +0200

    Implement indirect threading and add various macros. [vm]

--- vm/vm.c
+++ vm/vm.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
 #define STACK_SIZE 1<<20
-#define NEXT_INSTRUCTION goto next_instruction
+#define NEXT_INSTRUCTION goto *(labels[code[pc_x][pc_y]])
 #define ROWS 25
 #define COLS 80
 
-long long code[ROWS][COLS]; // the array is initialized to '\0'
-// we will treat '\0' as the space command, which does nothing,
-// in order to skip filling the whole array with spaces
+#define pop() (top==stack-1) ? 0 : *(top--)
+#define push(X) *(++top) = (X)
+
+long long code[ROWS][COLS];
 static long holdrand = 73L;
 signed long int stack[STACK_SIZE];
-
-int pc_x = 0, pc_y = 0; // row and collumn that the PC points to
-int x_direction = 0, y_direction = 1; // current direction in each axis
-int top = -1; // top points to the top element of the stack
-signed long int a,b; // local varriables to use for items that we pop out of the stack
-
+void * labels[256];
 
 void loader(){
   int i = 0, j = 0;
   char c;
+
+  for(int i = 0; i < ROWS; i++){
+    for(int j = 0; j < COLS; j++){
+      code[i][j] = ' ';
+    }
+  }
+
   while((c = getchar_unlocked()) != EOF){
     // printf("%c",c);
     if(c == '\n'){
@@ -33,26 +36,59 @@ void loader(){
   }
 }
 
+void fill_labels_table();
+
 int random(){
   return (((holdrand = holdrand * 214013L + 2531011L) >> 16) & 0x3);
   // returns a random number between 0 and 3
-}
-
-signed long int pop(){
-  signed long int p = (top==-1) ? 0 : stack[top--];
-  // printf("pop: %ld\n",p);
-  return p;
-}
-
-void push(signed long int x){
-  // printf("push: %ld\n",x);
-  stack[++top] = x;
 }
 
 int main(void){
   loader();
   char c;
   signed long int x;
+  register int pc_x = 0, pc_y = 0; // row and collumn that the PC points to
+  register int x_direction = 0, y_direction = 1; // current direction in each axis
+  register signed long int * top = stack-1; // top points to the top element of the stack
+  register signed long int a,b; // local varriables to use for items that we pop out of the stack
+
+  labels['0'] = &&label_0;
+  labels['1'] = &&label_1;
+  labels['2'] = &&label_2;
+  labels['3'] = &&label_3;
+  labels['4'] = &&label_4;
+  labels['5'] = &&label_5;
+  labels['6'] = &&label_6;
+  labels['7'] = &&label_7;
+  labels['8'] = &&label_8;
+  labels['9'] = &&label_9;
+  labels['+'] = &&label_add;
+  labels['-'] = &&label_subtract;
+  labels['*'] = &&label_multiply;
+  labels['/'] = &&label_divide;
+  labels['%'] = &&label_modulo;
+  labels['!'] = &&label_not;
+  labels['`'] = &&label_greater;
+  labels['>'] = &&label_right;
+  labels['<'] = &&label_left;
+  labels['^'] = &&label_up;
+  labels['v'] = &&label_down;
+  labels['?'] = &&label_random;
+  labels['_'] = &&label_horizontal_if;
+  labels['|'] = &&label_vertical_if;
+  labels['"'] = &&label_stringmode;
+  labels[':'] = &&label_dup;
+  labels['\\'] = &&label_swap;
+  labels['$'] = &&label_pop;
+  labels['.'] = &&label_output_int;
+  labels[','] = &&label_output_char;
+  labels['#'] = &&label_bridge;
+  labels['g'] = &&label_get;
+  labels['p'] = &&label_put;
+  labels['&'] = &&label_input_int;
+  labels['~'] = &&label_input_character;
+  labels[' '] = &&label_space;
+  labels['@'] = &&label_end;
 
   // TODO : implement direct threading & make PC a pointer
   // -- change pop implementation to help branch prediction on (top==-1)
@@ -283,71 +319,77 @@ next_instruction:
       NEXT_INSTRUCTION;
 
     case '0':
+    label_0:
       pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       push(0);
       NEXT_INSTRUCTION;
 
     case '1':
+    label_1:
       pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       push(1);
       NEXT_INSTRUCTION;
 
     case '2':
+    label_2:
       pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       push(2);
       NEXT_INSTRUCTION;
 
     case '3':
+    label_3:
       pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       push(3);
       NEXT_INSTRUCTION;
 
     case '4':
+    label_4:
       pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       push(4);
       NEXT_INSTRUCTION;
 
     case '5':
+    label_5:
       pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       push(5);
       NEXT_INSTRUCTION;
 
     case '6':
+    label_6:
       pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       push(6);
       NEXT_INSTRUCTION;
 
     case '7':
+    label_7:
       pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       push(7);
       NEXT_INSTRUCTION;
 
     case '8':
+    label_8:
       pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       push(8);
       NEXT_INSTRUCTION;
 
     case '9':
+    label_9:
       pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       push(9);
       NEXT_INSTRUCTION;
 
-    case ' ':
-      pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
-      pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
-      NEXT_INSTRUCTION;
-
-    case '\0':
+    case ' ': // space
+    label_space:
       pc_x = ((pc_x + x_direction)%ROWS + ROWS)%ROWS;
       pc_y = ((pc_y + y_direction)%COLS + COLS)%COLS;
       NEXT_INSTRUCTION;
@@ -358,4 +400,8 @@ next_instruction:
   }
 
   return 0;
+}
+
+void fill_labels_table(){
+
 }
