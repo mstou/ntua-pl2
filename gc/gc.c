96a77db2baea40e6fa38a7a51f254e4423e0f580
Miltiadis Stouras <m.stouras@gmail.com>
2019-12-24 20:03:45 +0200

    Add a heap. Implement the 3 extra instructions. Use the first 62bits of an ll for the numbers stored in the stackor the heap and use the 2 LSB for GC flags. All the befunge-snippets work in this commit but ping-pong fails.[GC]

--- gc/gc.c
+++ gc/gc.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
 #define STACK_SIZE 1<<20
+#define HEAP_SIZE  1<<24
 #define NEXT_INSTRUCTION goto *(labels[code[pc_x][pc_y]])
 #define ROWS 25
 #define COLS 80
 
 #define pop() (top==stack-1) ? 0 : *(top--)
-#define push(X) *(++top) = (X)
-#define increment_pc_x() {  pc_x += x_direction; if(pc_x == -1) pc_x = ROWS-1; else if(pc_x == ROWS) pc_x = 0;}
-#define increment_pc_y() {  pc_y += y_direction; if(pc_y == -1) pc_y = COLS-1; else if(pc_y == COLS) pc_y = 0;}
+#define push(X) *(++top) = (X);
+#define increment_pc_x() { pc_x += x_direction; if(pc_x == -1) pc_x = ROWS-1; else if(pc_x == ROWS) pc_x = 0;}
+#define increment_pc_y() { pc_y += y_direction; if(pc_y == -1) pc_y = COLS-1; else if(pc_y == COLS) pc_y = 0;}
 #define increment_pc() {increment_pc_x(); increment_pc_y();}
 
 long long code[ROWS][COLS];
 static long holdrand = 73L;
-signed long int stack[STACK_SIZE];
+long long int stack[STACK_SIZE];
+long long int heap[HEAP_SIZE];
+// 62bit integers -- we use the 2 LSBs for flags
+// integer[0] (LSB) = 1 iff this number is an address
+// integer[1] (LSB) is used by the GC
+
 void * labels[256];
+int m = 0;
 
 void loader(FILE* fp){
   int i = 0, j = 0;
@@ -47,13 +54,14 @@ int main(int argc, char* argv[]){
 
   FILE *fp = fopen(argv[1],"r");
   char c;
-  signed long int x;
+  long long int x;
   register int pc_x = 0;
   register int pc_y = 0; // row and collumn that the PC points to
   register int x_direction = 0;
   register int y_direction = 1; // current direction in each axis
-  register signed long int * top = stack-1; // top points to the top element of the stack
-  register signed long int a,b; // local varriables to use for items that we pop out of the stack
+  register long long int * top = stack-1; // top points to the top element of the stack
+  register int heap_top = -1; // pointer to the current heap limit
+  register long long int a,b; // local varriables to use for items that we pop out of the stack
 
   loader(fp);
 
@@ -93,6 +101,9 @@ int main(int argc, char* argv[]){
   labels['&'] = &&label_input_int;
   labels['~'] = &&label_input_character;
   labels[' '] = &&label_space;
+  labels['c'] = &&label_cons;
+  labels['h'] = &&label_head;
+  labels['t'] = &&label_tail;
   labels['@'] = &&label_end;
 
 next_instruction:
@@ -102,7 +113,7 @@ next_instruction:
       increment_pc();
       a = pop();
       b = pop();
-      push(b+a);
+      push( ((b>>2)+(a>>2)) << 2 );
       NEXT_INSTRUCTION;
 
     case '-': // subtract
@@ -110,7 +121,7 @@ next_instruction:
       increment_pc();
       a = pop();
       b = pop();
-      push(b-a);
+      push(((b>>2)-(a>>2)) << 2);
       NEXT_INSTRUCTION;
 
     case '*': // multiply
@@ -118,7 +129,7 @@ next_instruction:
       increment_pc();
       a = pop();
       b = pop();
-      push(b*a);
+      push(((b>>2)*(a>>2)) << 2);
       NEXT_INSTRUCTION;
 
     case '/': // divide
@@ -126,7 +137,7 @@ next_instruction:
       increment_pc();
       a = pop();
       b = pop();
-      push(b/a);
+      push(((b>>2)/(a>>2)) << 2);
       NEXT_INSTRUCTION;
 
     case '%': // modulo
@@ -134,14 +145,15 @@ next_instruction:
       increment_pc();
       a = pop();
       b = pop();
-      push(b%a);
+      push(((b>>2)%(a>>2)) << 2);
       NEXT_INSTRUCTION;
 
     case '!': // not
     label_not:
       increment_pc();
       a = pop();
-      push((a!=0) ? 0 : 1);
+      // printf("a = %d, a>>2 = %d\n",a,a>>2);
+      push((((a>>2)!=0) ? 0 : 1)<<2);
       NEXT_INSTRUCTION;
 
     case '`': // greater
@@ -149,7 +161,7 @@ next_instruction:
       increment_pc();
       a = pop();
       b = pop();
-      push((b > a) ? 1 : 0);
+      push((((b>>2) > (a>>2)) ? 1 : 0)<<2);
       NEXT_INSTRUCTION;
 
     case '>': // right
@@ -197,13 +209,13 @@ next_instruction:
     case '_': // horizontal if
     label_horizontal_if:
       a = pop();
-      if(a == 0) goto label_right;
+      if((a>>2) == 0) goto label_right;
       else goto label_left;
 
     case '|': // vertical if
     label_vertical_if:
       a = pop();
-      if(a == 0) goto label_down;
+      if((a>>2) == 0) goto label_down;
       else goto label_up;
 
     case '"': // stringmode
@@ -212,7 +224,7 @@ next_instruction:
 
       c = code[pc_x][pc_y];
       if( c != '"'){
-        push(c);
+        push(c<<2);
         goto label_stringmode;
       }
       increment_pc();
@@ -245,14 +257,16 @@ next_instruction:
     label_output_int:
       increment_pc();
       a = pop();
-      printf("%ld ",a);
+      printf("%lld ",a>>2);
+      fflush(stdout);
       NEXT_INSTRUCTION;
 
     case ',': // output char
     label_output_char:
       increment_pc();
       a = pop();
-      printf("%c", (char) a);
+      printf("%c", (char) (a>>2));
+      fflush(stdout);
       NEXT_INSTRUCTION;
 
     case '#': // bridge
@@ -266,7 +280,7 @@ next_instruction:
       increment_pc();
       a = pop();
       b = pop();
-      push(code[a][b]);
+      push(code[(a>>2)][(b>>2)]<<2);
       NEXT_INSTRUCTION;
 
     case 'p': // put
@@ -275,86 +289,121 @@ next_instruction:
       a = pop();
       b = pop();
       x = pop();
-      code[a][b] = x;
+      code[(a>>2)][(b>>2)] = (x>>2);
       NEXT_INSTRUCTION;
 
     case '&': // input int
     label_input_int:
       increment_pc();
-      scanf("%ld",&x);
-      push(x);
+      scanf("%lld",&x);
+      push(x<<2);
       NEXT_INSTRUCTION;
 
     case '~': // input_character
     label_input_character:
       increment_pc();
       scanf("%c",&c);
-      push(c);
+      push(c<<2);
       NEXT_INSTRUCTION;
 
     case '0':
     label_0:
       increment_pc();
-      push(0);
+      push(0<<2);
       NEXT_INSTRUCTION;
 
     case '1':
     label_1:
       increment_pc();
-      push(1);
+      push(1<<2);
       NEXT_INSTRUCTION;
 
     case '2':
     label_2:
       increment_pc();
-      push(2);
+      push(2<<2);
       NEXT_INSTRUCTION;
 
     case '3':
     label_3:
       increment_pc();
-      push(3);
+      push(3<<2);
       NEXT_INSTRUCTION;
 
     case '4':
     label_4:
       increment_pc();
-      push(4);
+      push(4<<2);
       NEXT_INSTRUCTION;
 
     case '5':
     label_5:
       increment_pc();
-      push(5);
+      push(5<<2);
       NEXT_INSTRUCTION;
 
     case '6':
     label_6:
       increment_pc();
-      push(6);
+      push(6<<2);
       NEXT_INSTRUCTION;
 
     case '7':
     label_7:
       increment_pc();
-      push(7);
+      push(7<<2);
       NEXT_INSTRUCTION;
 
     case '8':
     label_8:
       increment_pc();
-      push(8);
+      push(8<<2);
       NEXT_INSTRUCTION;
 
     case '9':
     label_9:
       increment_pc();
-      push(9);
+      push(9<<2);
       NEXT_INSTRUCTION;
 
     case ' ': // space
     label_space:
       increment_pc();
+      NEXT_INSTRUCTION;
+
+    case 'c': // cons
+    label_cons:
+      increment_pc();
+
+      b = pop();
+      a = pop();
+      heap[++heap_top] = a;
+
+      long long int addr = (heap_top << 2) | 0x1;
+      push(addr);
+
+      heap[++heap_top] = b;
+      NEXT_INSTRUCTION;
+
+    case 'h': // head
+    label_head:
+      increment_pc();
+      a = pop();
+      if(!(a & 0x1)){
+        printf("Warning! Not an address!\n");
+      }
+
+      push(heap[a>>2]);
+      NEXT_INSTRUCTION;
+
+    case 't': // tail
+    label_tail:
+      increment_pc();
+      a = pop();
+      if(!(a & 0x1)){
+        printf("Warning! Not an address!\n");
+      }
+      push(heap[(a>>2)+1]);
       NEXT_INSTRUCTION;
 
     case '@': // end
