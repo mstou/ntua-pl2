4579825980ca519d48ab4251ececda24d2fd2dc7
Miltiadis Stouras <m.stouras@gmail.com>
2020-09-03 20:23:44 +0300

    Read input and print answered querries. [dp]

--- haskell-dp/dp.hs
+++ haskell-dp/dp.hs
@@ -1,39 +1,55 @@
 import Data.List (foldl', scanl')
 import Data.Map.Strict (fromDistinctAscList)
 import qualified Data.Map.Strict as Map
+import qualified Data.ByteString.Char8 as C
+import Data.ByteString.Lazy.Builder
+import Data.ByteString.Lazy.Builder.ASCII
+import Data.Monoid
+import Data.Int
+import Control.Monad (replicateM)
+import System.IO
 
-p = 1000000007
 maxElement = 1000001
 -- sums = takeWhile (<maxElement) $ scanl1 (+) $ map (floor . (2**)) [0..]
 sums = [1,3,7,15,31,63,127,255,511,1023,2047,4095,8191,16383,32767,65535,131071,262143,524287]
 
-sum' = foldl' (\acc x -> (acc + x) `mod` p) 0
+sum' p = foldl' (\acc x -> (acc + x) `mod` p) 0
 
 addNewTerm x = x `elem` sums
 
 -- dp[1], dp[2], ...
-dp = 2 : 2 : sumHeads 2 [tail dp]
+calculateDpList p = dp
   where
-    sumHeads x lists =
-      heads `seq` sumOfHeads `seq` tails `seq` newSumTerms `seq`
-      (sumOfHeads : sumHeads (x+1) newSumTerms)
-        where heads = map head lists
-              sumOfHeads = (sum' heads) `mod` p
-              tails = map tail lists
-              newSumTerms = if (addNewTerm $ x+1) then (dp : tails) else tails
+        dp = 2 : 2 : sumHeads 2 [tail dp]
+          where
+            sumHeads x lists =
+              heads `seq` sumOfHeads `seq` tails `seq` newSumTerms `seq`
+              (sumOfHeads : sumHeads (x+1) newSumTerms)
+                where heads = map head lists
+                      sumOfHeads = ((sum' p) heads) `mod` p
+                      tails = map tail lists
+                      newSumTerms = if (addNewTerm $ x+1) then (dp : tails) else tails
 
 
-mapOfPrefixSums = fromDistinctAscList $! prefixSumsWithKeys
+getMapOfPrefixSums dp p = fromDistinctAscList $! prefixSumsWithKeys
   where prefixSumsWithKeys = take maxElement $ zip [0..] prefixSums
         prefixSums = scanl1 (\x y -> x `seq` (x+y) `mod` p) dp
 
-answerQuerry x y =
-  prefixSumX `seq` prefixSumY `seq` (prefixSumY - prefixSumX) `mod` p
+answerQuerry mapOfPrefixSums p (x,y) =
+  mapOfPrefixSums `seq` prefixSumX `seq` prefixSumY `seq` (prefixSumY - prefixSumX) `mod` p
   where (Just prefixSumX) = if x == 0 then (Just 1) else Map.lookup (x-1) mapOfPrefixSums
         (Just prefixSumY) = Map.lookup y mapOfPrefixSums
 
+readInts :: IO [Int64]
+readInts = map parse . C.words <$> C.getLine
+  where parse s = let Just (n, _) = C.readInteger s
+                    in fromIntegral n
 main = do
-  print(answerQuerry 0 10)
-  print(answerQuerry 1742 4217)
-  print(answerQuerry 500000 999999)
-  -- mapOfPrefixSums `seq` print (dp !! 1000000)
+  (n:m:[]) <- readInts
+  querries <- replicateM (fromIntegral n) readInts
+  mapOfPrefixSums <- pure $ getMapOfPrefixSums (calculateDpList m) m
+  answers <- pure $ map ( (answerQuerry mapOfPrefixSums m) . toTuple ) querries
+  hPutBuilder stdout $ unlines_ $ map int64Dec (answers::[Int64])
+    where
+      unlines_ = mconcat . map (<> charUtf8 '\n')
+      toTuple = (\(x:y:[]) -> (x,y))
